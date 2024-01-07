e8a88d2980654f784d7810e7f8c50171dbf338d4
JethroNatividad <natividad.jet@gmail.com>
2024-01-07 15:03:57 +0800

    write tests

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,26 @@
 // Process: checks if legal age
 // Outputs: “You are old enough to legally drive.” or “You are not old enough to legally drive.”
 
+fn can_drive(age: i32) -> bool {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_can_drive() {
+        assert_eq!(can_drive(1), false);
+        assert_eq!(can_drive(15), false);
+        assert_eq!(can_drive(16), true);
+        assert_eq!(can_drive(30), true);
+        assert_eq!(can_drive(0), false);
+        assert_eq!(can_drive(3252340), true);
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
