3640715abbe7f211100b6bf0129adc9cb52d2c47
JethroNatividad <natividad.jet@gmail.com>
2024-01-07 15:08:48 +0800

    write pseudocode

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 // program that prompts for age, and checks if it is under or over the legal age.
 // Inputs: age
 // Process: checks if legal age
-// Outputs: “You are old enough to legally drive.” or “You are not old enough to legally drive.”
+// Outputs: "You are old enough to legally drive." or "You are not old enough to legally drive."
 
 fn can_drive_legally(age: i32) -> bool {
     age >= 16
@@ -41,5 +41,10 @@ fn get_input<T: std::str::FromStr>(prompt: &str) -> T {
 }
 
 fn main() {
-    println!("Hello, world!");
+    // Set legal age to 16
+    // prompt for input_age "What is your age? "
+    // Check if age is legal
+        // if legal, print "You are old enough to legally drive."
+        // if not, print  "You are not old enough to legally drive."
+    
 }
