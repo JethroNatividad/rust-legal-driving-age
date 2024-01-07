8dbfc5393ae5c54776dcdb0949e159784838886e
JethroNatividad <natividad.jet@gmail.com>
2024-01-07 15:00:54 +0800

    expand output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 // program that prompts for age, and checks if it is under or over the legal age.
-
+// Inputs: age
+// Process: checks if legal age
+// Outputs: “You are old enough to legally drive.” or “You are not old enough to legally drive.”
 
 fn main() {
     println!("Hello, world!");
